3dd69a059de751938697138bac4601be913b2eb0
Kalpita Birhade <84992682+kalpitabirhade28@users.noreply.github.com>
2024-12-19 18:26:06 +0530

    Update provider.go

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -1,115 +1,22 @@
 package provider
 
 import (
-	"context"
-	"os"
-
-	"github.com/hashicorp/terraform-plugin-framework/datasource"
-	"github.com/hashicorp/terraform-plugin-framework/path"
-	"github.com/hashicorp/terraform-plugin-framework/provider"
-	"github.com/hashicorp/terraform-plugin-framework/provider/schema"
-	"github.com/hashicorp/terraform-plugin-framework/resource"
-	"github.com/hashicorp/terraform-plugin-framework/types"
-	"github.com/netdata/terraform-provider-netdata/internal/client"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-var _ provider.Provider = &netdataCloudProvider{}
-
-const NetdataCloudURL = "https://app.netdata.cloud"
-
-type netdataCloudProvider struct {
-	version string
-}
-
-type netdataCloudProviderModel struct {
-	Url       types.String `tfsdk:"url"`
-	AuthToken types.String `tfsdk:"auth_token"`
-}
-
-func (p *netdataCloudProvider) Metadata(ctx context.Context, req provider.MetadataRequest, resp *provider.MetadataResponse) {
-	resp.TypeName = "netdata"
-	resp.Version = p.version
-}
-
-func (p *netdataCloudProvider) Schema(ctx context.Context, req provider.SchemaRequest, resp *provider.SchemaResponse) {
-	resp.Schema = schema.Schema{
-		Description: "The Netdata Provider allows you to manage Netdata Cloud resources.",
-		Attributes: map[string]schema.Attribute{
-			"url": schema.StringAttribute{
-				MarkdownDescription: "Netdata Cloud URL Address by default is https://app.netdata.cloud. Can be also set as environment variable `NETDATA_CLOUD_URL`",
-				Optional:            true,
-			},
-			"auth_token": schema.StringAttribute{
-				MarkdownDescription: "Netdata Cloud Authentication Token with `scope:all`, more [info](https://learn.netdata.cloud/docs/netdata-cloud/api-tokens). Can be also set as environment variable `NETDATA_CLOUD_AUTH_TOKEN`",
-				Sensitive:           true,
-				Optional:            true,
+// Provider function for Netdata
+func Provider() *schema.Provider {
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"api_key": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "API key for Netdata Cloud authentication",
 			},
 		},
-	}
-}
-
-func (p *netdataCloudProvider) Configure(ctx context.Context, req provider.ConfigureRequest, resp *provider.ConfigureResponse) {
-	var data netdataCloudProviderModel
-
-	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
-
-	if resp.Diagnostics.HasError() {
-		return
-	}
-
-	url := os.Getenv("NETDATA_CLOUD_URL")
-	auth_token := os.Getenv("NETDATA_CLOUD_AUTH_TOKEN")
-
-	if !data.AuthToken.IsNull() {
-		auth_token = data.AuthToken.ValueString()
-	}
-
-	if !data.Url.IsNull() {
-		url = data.Url.ValueString()
-	}
-
-	if url == "" {
-		url = NetdataCloudURL
-	}
-
-	if auth_token == "" {
-		resp.Diagnostics.AddAttributeError(
-			path.Root("auth_token"),
-			"Missing Netdata Cloud Authentication Token",
-			"Provide a valid Netdata Cloud Authentication Token to authenticate with Netdata Cloud.",
-		)
-	}
-
-	client := client.NewClient(url, auth_token)
-
-	resp.DataSourceData = client
-	resp.ResourceData = client
-}
-
-func (p *netdataCloudProvider) Resources(ctx context.Context) []func() resource.Resource {
-	return []func() resource.Resource{
-		NewSpaceResource,
-		NewRoomResource,
-		NewSpaceMemberResource,
-		NewRoomMemberResource,
-		NewSlackChannelResource,
-		NewDiscordChannelResource,
-		NewPagerdutyChannelResource,
-		NewNodeRoomMemberResource,
-	}
-}
-
-func (p *netdataCloudProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
-	return []func() datasource.DataSource{
-		NewSpaceDataSource,
-		NewRoomDataSource,
-	}
-}
-
-func New(version string) func() provider.Provider {
-	return func() provider.Provider {
-		return &netdataCloudProvider{
-			version: version,
-		}
+		ResourcesMap: map[string]*schema.Resource{
+			"netdata_dashboard": resourceDashboard(),
+			"netdata_server":    resourceServer(),
+		},
 	}
 }
